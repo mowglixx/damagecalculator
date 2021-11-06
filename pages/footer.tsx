61e242200af8b74fd2f4326b4afe564d6444c3d2
Daniel Monaghan <mowglixx@users.noreply.github.com>
2021-11-06 14:32:10 +0000

    customised template

--- pages/footer.tsx
+++ pages/footer.tsx
@@ -3,12 +3,11 @@ export default function Footer(){
         <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/mowglixx"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          GitHub
         </a>
       </footer>
     )
--- pages/myAccount/dash/index.tsx
+++ pages/myAccount/dash/index.tsx
@@ -1,9 +1,9 @@
 function index() {
     return (
-        <div className="absolute p-20">
-            <p className="mt-5">Dear John,</p>
-            <p className="mt-5 ">It has been a long time...</p>
-            <p className="mt-5">Yours,<br />Dan</p>
+        <div className="absolute m-20">
+            <p className="m-5">Dear John,</p>
+            <p className="m-5 ">It has been a long time...</p>
+            <p className="m-5">Yours,<br />Dan</p>
         </div>
     )
 }
