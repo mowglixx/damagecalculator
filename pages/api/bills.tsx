008b162be21d7d380c64e50dee4cf380c32458a4
Daniel Monaghan <mowglixx@users.noreply.github.com>
2021-11-09 02:04:12 +0000

    defined types for bills

--- pages/api/bills.tsx
+++ pages/api/bills.tsx
@@ -1,1 +1,39 @@
-export {}
+const exampleBills: Bills = [
+    {
+        name: 'TalkTalk',
+        billCatagory: 'Utilities',
+        amount: 26.50,
+        frequency: 'weekly'
+    },
+    {
+        name: 'British Gas',
+        billCatagory: 'Utilities',
+        amount: 49.50,
+        frequency: 'weekly'
+    },
+    {
+        name: 'Nintendo',
+        billCatagory: 'Gaming',
+        amount: 9.99,
+        frequency: 'weekly'
+    },
+    {
+        name: 'ID Mobile',
+        billCatagory: 'Utilities',
+        amount: 21.99,
+        frequency: 'weekly'
+    }
+]
+
+
+export type Bill = {
+    name: string,
+    billCatagory: string,
+    amount: number,
+    frequency: string
+}
+export type Bills = Bill[]
+
+export default function handler({ query, headers }, res) {
+    res.status(200).json({ query, headers, exampleBills })
+}
